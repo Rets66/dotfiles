cb7f34c31dc1268504387cb3ab4be047d70d9f37
Rets66 <takasuka_shoretsu@cyberagent.co.jp>
2021-11-18 23:13:13 +0900

    update

--- bin/init.sh
+++ bin/init.sh
@@ -5,12 +5,29 @@
 # Set up Rust Language
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# Update Homebrew
+brew update
+brew upgrade
+
 # Install tools
-brew install ripgrep jq tree
-brew install kubectl kustomize
-cargo install git-delta
+brew install ripgrep
+brew install jq
+brew install tree
+brew install kubectl
+brew install kustomize
+brew install go
+brew install ranger
+brew install tfenv
+brew install curl
+brew install wget
+brew install neovim
 
 # Install Apps
-brew install go
 brew cask install firefox
+brew cask install thunderbird
+brew cask install slack
+brew cask install karabiner-elements
+brew cask install zoom
 
+# Other
+cargo install git-delta
